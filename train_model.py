6f5a52c2d8f92f6299f19a0a09479b96d6d5ad37
arshin0 <105578463+arshin0@users.noreply.github.com>
2026-02-20 18:59:09 +0530

    Add files via upload

---
+++ train_model.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import joblib
+
+# Load dataset
+data = pd.read_csv("../dataset/attendance_dataset.csv")
+
+X = data[['total_classes', 'attended', 'previous_attendance', 'bunk_rate']]
+y = data['below_75']
+
+# Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Accuracy
+y_pred = model.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+
+# Save model
+joblib.dump(model, "../attendance_model.pkl")
+
+print("Model saved successfully!")
